c2aac0becafa99a4806671eb7560cbfa87641180
maplesift <a23050716@gmail.com>
2025-01-09 10:54:14 +0800

    z級03/建立預告片後台管理頁面表單

--- back/poster.php
+++ back/poster.php
@@ -1,1 +1,22 @@
-post
+<div style="height:340px;">123</div>
+<hr>
+<div style="height:170px;">
+    <div class="ct post">新增預告片海報</div>
+    <form action="./apo/add_poster.php" method="post" enctype="multipart/form-data">
+
+        <table>
+            <tr>
+                <td>預告片海報:</td>
+                <td><input type="file" name="img" id=""></td>
+                <td>預告片片名:</td>
+                <td><input type="text" name="name" id=""></td>
+            </tr>
+        </table>
+        <div class="ct">
+            <input type="submit" value="新增">
+            <input type="reset" value="重置">
+        </div>
+    </form>
+
+
+</div>
