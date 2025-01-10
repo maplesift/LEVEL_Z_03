b8b226ccabe7a4c007710318209e08a58a6532d0
maplesift <a23050716@gmail.com>
2025-01-10 11:51:59 +0800

    z級03:完成院線片清單顯示功能

--- front/main.php
+++ front/main.php
@@ -22,16 +22,55 @@
     $start=($now-1)*$div;
 
     $rows=$Movie->all(['sh'=>1]," AND ondate BETWEEN '$ondate' AND '$today' order by rank limit $start,$div");
-     
-    dd($rows);
+    
+    // dd($rows);
     ?>
 
     <div class="rb tab" style="width:95%;">
-        <table>
-            <tbody>
-                <tr> </tr>
-            </tbody>
-        </table>
-        <div class="ct"> </div>
+        <div style="display:flex;flex-wrap:wrap;">
+            <?php foreach ($rows as $row): ?>
+            <div class="movie-item">
+
+                <div style="width:65px">
+                    <img src="./upload/<?=$row['poster'];?>" style="width: 60px;height: 80px;">
+                </div>
+                <div style="width:calc(100% - 65px);">
+                    <div>片名:<?=$row['name'];?></div>
+                    <div>分級:
+                        <img src="./icon/03C0<?=$row['level'];?>.png" style="width:20px;vertical-align:middle">
+                        <?=$Movie::$level[$row['level']];?>
+                    </div>
+                    <div>上映日期:<?=$row['ondate'];?></div>
+                </div>
+                <div>
+                    <button> 劇情簡介</button>
+                    <button> 線上訂票</button>
+                </div>
+            </div>
+            <?php endforeach; ?>
+        </div>
+        <div class="ct">
+            <!-- 分頁 -->
+            <?php
+
+                if(($now-1)>0){
+                    echo "<a href='?p=".($now-1)."' > < </a>";
+                }
+                
+                for($i=1;$i<=$pages;$i++){
+                    $fontsize=($i==$now)?'24px':'18px';
+                    echo "<a href='?p=$i' style='font-size:$fontsize'>$i</a>";
+                }
+                
+                if(($now+1)<=$pages){
+                    echo "<a href='?p=".($now+1)."' > > </a>";
+                }
+
+
+            ?>
+        </div>
     </div>
 </div>
+<style>
+
+</style>
