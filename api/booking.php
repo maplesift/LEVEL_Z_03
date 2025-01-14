404ec3b29924e3911380ebd75286e7eface403f8
maplesift <a23050716@gmail.com>
2025-01-14 09:51:47 +0800

    z級03:增加顯示勾選票數/完成勾選座位功能

--- api/booking.php
+++ api/booking.php
@@ -5,7 +5,7 @@
 for ($i=0; $i <20 ; $i++) { 
     echo "<div class='seat null'>";
     echo floor($i/5)+1 ."排".($i%5+1)."號";
-    echo "<input type='checkbox' value='$i' >";
+    echo "<input type='checkbox' value='$i' class='chk' >";
     echo "</div>";
 }
 ?>
@@ -13,10 +13,32 @@ for ($i=0; $i <20 ; $i++) {
 <div id="movieInfo">
     <div>您選擇的電影是：<?=$_GET['name'];?></div>
     <div>您選擇的時刻是：<?=$_GET['date']."&nbsp;&nbsp;".$_GET['session'];?></div>
-    <div>您已經<span id='tickets'></span>勾選張票，最多可以購買四張票</div>
+    <div>您已經勾選<span id='tickets'></span>張票，最多可以購買四張票</div>
     <div class='ct'>
         <button onclick="$('#booking,#order').toggle()">上一步</button>
         <button onclick="checkout()">訂購</button>
     </div>
-
 </div>
+
+<script>
+let seats = new Array();
+
+$(".chk").on("change", function() {
+    if ($(this).prop('checked')) {
+        if (seats.length > 3) {
+            alert("最多只能選四張票");
+            $(this).prop('checked', false);
+        } else {
+            // 把 值丟入 陣列seats
+            seats.push($(this).val());
+        }
+
+    } else {
+        // 把值 從seats移出
+        seats.splice(seats.indexOf($(this).val()), 1) // 1=>刪除的個數
+    }
+    $("#tickets").text(seats.length)
+    // console.log(seats);
+
+})
+</script>
